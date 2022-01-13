951bbc9384e0b3568317d8cbd0183630711ece69
tanguybro <tanguy.brouassin@gmail.com>
2022-01-13 16:07:22 +0100

    add election contract

---
+++ contracts/Election.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.5.16;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Read/write Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Constructor
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+}
